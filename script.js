bec5564799ccec41f45b7e5eaf77ae9af2ec00ec
Suryansh Kumar Srivastava <83886721+suryansh1605@users.noreply.github.com>
2023-09-09 10:54:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function bindData(articles) {
 
     cardsContainer.innerHTML = "";
 
-    articles.forEach((article) => {
+   articles && articles.forEach((article) => {
         if (!article.urlToImage) return;
         const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
